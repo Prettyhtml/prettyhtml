274f32ed2576692a589e51322dfc19ba12c16bc4
starptech <deusdustin@gmail.com>
2018-06-24 12:53:45 +0200

    ...

--- examples/simple.js
+++ examples/simple.js
@@ -1,7 +1,8 @@
 const prettyhtml = require('./../packages/prettyhtml')
 
 // example with angular template
-const result = prettyhtml(`<my-component ng-model="selected" disabled="AAA">
+const result = prettyhtml(`
+<my-component ng-model="selected" disabled="AAA">
 <custom FOO="test" class="test" title="wdwe"></custom>
 <form #heroForm (ngSubmit)="onSubmit(heroForm)">
   <input type="text" [(onChange)]="dede" name="test"/>
@@ -14,6 +15,7 @@ const result = prettyhtml(`<my-component ng-model="selected" disabled="AAA">
     <section>bar</section>
     <my-sub-sub-component ng-repeat="item in items" ng-value="item.value" aria-label="{{item.label}}">{{ item.label }}</my-sub-sub-component>
   </my-sub-component>
-  <div></div></my-component>`)
+  <div></div></my-component>
+`)
 
 console.log(result.contents)
