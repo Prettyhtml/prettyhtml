fdd4d47495059c8e3600781849cdf5f55be60994
starptech <deusdustin@gmail.com>
2018-07-10 21:06:25 +0200

    improve tests

--- examples/simple.js
+++ examples/simple.js
@@ -1,13 +1,12 @@
 const prettyhtml = require('./../packages/prettyhtml')
 
 // example with angular template
-const result = prettyhtml(`<form>
-<div test="dddddddd
-ddddddd" id="dwedwedwed"><p>foo</p></div>
+const result = prettyhtml(`<!-- custom -->
+<my-component ng-model="selected"><custom FOO="test" class="test" title="title"></custom>
+<form #heroForm (ngSubmit)="onSubmit(heroForm)" title="title" data-foo="foo" data-baz="baz"> <input
+type="text" [(onChange)]="dede" name="test"> <button [style.color]="isSpecial ? 'red' : 'green'"></button>
+<div>Foo bar test</div>
 </form>
-<form>
-<div test="dddddddd
-ddddddd"><p>foo</p></div>
-</form>`)
+</my-component>`)
 
 console.log(result.contents)
