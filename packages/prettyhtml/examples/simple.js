68b806c1c4bbf53f296d9796524968a0189771ae
starptech <deusdustin@gmail.com>
2018-08-20 22:40:53 +0200

    restore example, rename test

--- packages/prettyhtml/examples/simple.js
+++ packages/prettyhtml/examples/simple.js
@@ -2,6 +2,6 @@ const prettyhtml = require('..')
 const { writeFileSync, readFileSync } = require('fs')
 
 // example with angular template syntax
-const result = prettyhtml(readFileSync('./examples/angular.html'))
+const result = prettyhtml(readFileSync('./examples/simple.html'))
 
 writeFileSync('./examples/result.html', result.contents, 'utf8')
