9b38df43ec54124823accbc74305962af221baef
starptech <deusdustin@gmail.com>
2018-08-26 20:40:41 +0200

    fix tests

--- packages/prettyhtml-hastscript/test.js
+++ packages/prettyhtml-hastscript/test.js
@@ -537,10 +537,10 @@ test('hastscript', function(t) {
         {
           type: 'element',
           tagName: 'div',
-          properties: { download: true },
+          properties: { download: 'downLOAD' },
           children: []
         },
-        'should cast known named overloaded `boolean` values'
+        'should not cast known named overloaded `boolean` values'
       )
 
       st.deepEqual(
@@ -909,8 +909,7 @@ test('hastscript', function(t) {
         type: 'element',
         tagName: 'template',
         properties: {},
-        children: [],
-        content: { type: 'root', children: [] }
+        children: []
       },
       'empty template'
     )
@@ -921,8 +920,7 @@ test('hastscript', function(t) {
         type: 'element',
         tagName: 'template',
         properties: {},
-        children: [],
-        content: { type: 'root', children: [{ type: 'text', value: 'Alpha' }] }
+        children:  [{ type: 'text', value: 'Alpha' }]
       },
       'template with text'
     )
@@ -933,26 +931,22 @@ test('hastscript', function(t) {
         type: 'element',
         tagName: 'template',
         properties: {},
-        children: [],
-        content: {
-          type: 'root',
-          children: [
-            {
-              type: 'element',
-              tagName: 'b',
-              properties: {},
-              children: [{ type: 'text', value: 'Bold' }]
-            },
-            { type: 'text', value: ' and ' },
-            {
-              type: 'element',
-              tagName: 'i',
-              properties: {},
-              children: [{ type: 'text', value: 'italic' }]
-            },
-            { type: 'text', value: '.' }
-          ]
-        }
+        children: [
+          {
+            type: 'element',
+            tagName: 'b',
+            properties: {},
+            children: [{ type: 'text', value: 'Bold' }]
+          },
+          { type: 'text', value: ' and ' },
+          {
+            type: 'element',
+            tagName: 'i',
+            properties: {},
+            children: [{ type: 'text', value: 'italic' }]
+          },
+          { type: 'text', value: '.' }
+        ]
       },
       'template with elements'
     )
