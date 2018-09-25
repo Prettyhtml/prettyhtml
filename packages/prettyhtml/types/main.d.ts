5d5df972ca6dc3093531976c6f6a091997ecbf5c
Pine Wu <octref@gmail.com>
2018-09-25 15:08:32 -0700

    Add types

---
+++ packages/prettyhtml/types/main.d.ts
@@ -0,0 +1,9 @@
+declare module '@starptech/prettyhtml' {
+  export default function prettyhtml(input: string, options: {
+    tabWidth?: number,
+    useTabs?: boolean,
+    printWidth?: number,
+    usePrettier?: boolean,
+    singleQuote?: boolean
+  }): string;
+}
