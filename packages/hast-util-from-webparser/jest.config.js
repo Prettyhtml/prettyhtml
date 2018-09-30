289f7840f251cf89df4eb929085b4cad0a6d1f72
starptech <deusdustin@gmail.com>
2018-09-30 21:08:31 +0200

    update deps

--- packages/hast-util-from-webparser/jest.config.js
+++ packages/hast-util-from-webparser/jest.config.js
@@ -8,6 +8,6 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
-  testMatch: ['**/test/**/*.test.(ts|js)'],
+  testMatch: ['**/test/**/*.test.(ts|js)', '**/__snapshot__/**/*.snap'],
   testEnvironment: 'node'
 }
