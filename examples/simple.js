e56de02e1e43ca37440807db149efd39f001bb78
starptech <deusdustin@gmail.com>
2018-10-01 22:20:57 +0200

    restore old example

--- examples/simple.js
+++ examples/simple.js
@@ -2,18 +2,7 @@ const prettyhtml = require('./../packages/prettyhtml')
 
 // example with angular template
 const result = prettyhtml(
-  `<div id="app">
-  <div class="header">
-<button @click="prettify">Prettify</button>
-</div>
-  <div style="display:flex">
-<prism-editor class="my-editor" :code="code" @change="changeCode" language="html" />
-<prism-editor :code="result" language="html" />
-</div>
-</div>`,
-  {
-    useTabs: true
-  }
+  `<form #heroForm (ngSubmit)="onSubmit(heroForm)"><input type="text" [(onChange)]="dede" name="test" /><button [style.color]="isSpecial ? 'red' : 'green'"></button></form>`
 )
 
 console.log(result)
