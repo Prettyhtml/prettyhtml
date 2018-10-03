9182ad6d9a84c4640a129b2e0c9871b73d016d80
starptech <deusdustin@gmail.com>
2018-10-03 22:38:25 +0200

    remove examples, gitignore

--- packages/prettyhtml/examples/simple.js
+++
@@ -1,7 +0,0 @@
-const prettyhtml = require('..')
-const { writeFileSync, readFileSync } = require('fs')
-
-// example with angular template syntax
-const result = prettyhtml(readFileSync('./examples/simple.html'))
-
-writeFileSync('./examples/result.html', result, 'utf8')
