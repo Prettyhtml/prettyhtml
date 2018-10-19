7e78ab386be52a8ac9e61f952055af0a2b9436a7
starptech <deusdustin@gmail.com>
2018-10-19 14:27:38 +0200

    handle parse errors as fatal and warning, use shortener error message

--- packages/rehype-webparser/index.ts
+++ packages/rehype-webparser/index.ts
@@ -1,4 +1,4 @@
-import { ParseError, HtmlParser } from '@starptech/webparser'
+import { HtmlParser, ParseErrorLevel } from '@starptech/webparser'
 import fromWebparser from '@starptech/hast-util-from-webparser'
 
 interface ParseOptions {
@@ -9,6 +9,7 @@ interface ParseOptions {
 interface VFile {
   path: string
   message(reason: string, position: any, origin: string): void
+  fail(reason: string, position: any, origin: string): void
 }
 
 export = function parse(options: ParseOptions = {}): any {
@@ -17,9 +18,30 @@ export = function parse(options: ParseOptions = {}): any {
   function parser(doc: string, file: VFile) {
     const parseResult = new HtmlParser(options).parse(doc, file.path)
 
-    for (const err of parseResult.errors) {
+    for (const err of parseResult.errors.filter(e => e.level === ParseErrorLevel.WARNING)) {
       file.message(
-        err.contextualMessage(),
+        err.msg,
+        {
+          start: {
+            // webparser format counts lines beginning from zero
+            line: ++err.span.start.line,
+            offset: err.span.start.offset,
+            column: err.span.start.col
+          },
+          end: {
+            line: ++err.span.end.line,
+            offset: err.span.end.offset,
+            column: err.span.end.col
+          }
+        },
+        'ParseError'
+      )
+    }
+
+    // log the last error because the lexer will throw at first with a less meaningful error message
+    for (const err of parseResult.errors.filter(e => e.level === ParseErrorLevel.ERROR).reverse()) {
+      file.fail(
+        err.msg,
         {
           start: {
             // webparser format counts lines beginning from zero
