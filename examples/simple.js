d5122e5d45fc5f747eb755787d9abe51114d9a87
dustin deus <deusdustin@gmail.com>
2018-12-04 00:12:23 +0100

    align first text node

--- examples/simple.js
+++ examples/simple.js
@@ -2,9 +2,7 @@ const prettyhtml = require('./../packages/prettyhtml')
 
 // example with angular template
 try {
-  const result = prettyhtml(
-    `<form #heroForm (ngSubmit)="onSubmit(heroForm)"><input type="text" [(onChange)]="dede" name="test" /><button [style.color]="isSpecial ? 'red' : 'green'"></button></form>`
-  )
+  const result = prettyhtml(`<p>dwedwedw</p>`)
   console.log(result.contents)
 } catch (error) {
   console.error(error)
