416683dc9d9671a8f875d5edd2d24251028ef57b
Phil Marshall <phillipsolomonmarshall@gmail.com>
2018-12-06 17:08:56 -0600

    add first test for cli

---
+++ packages/prettyhtml/test/cli.js
@@ -0,0 +1,46 @@
+const spawn = require('child_process').spawn
+const test = require('ava')
+const fs = require('fs')
+const path = require('path')
+const dir = path.join(__dirname, './tmp')
+try {
+  fs.mkdirSync(dir)
+} catch (err) {
+  if (err.code !== 'EEXIST') throw err
+}
+
+test.cb('cli', t => {
+  fs.writeFile(
+    path.join(dir, 'single-quote.html'),
+    '<div id="foo"/>\n',
+    err => {
+      if (err) throw err
+      const cp = spawn('./cli.js', [
+        'test/tmp/single-quote.html',
+        '--single-quote'
+      ])
+      cp.on('close', function(code) {
+        t.is(
+          fs.readFileSync(path.join(dir, 'single-quote.html'), 'utf8'),
+          "<div id='foo'/>\n",
+          'double quotes should be replaced with single quotes'
+        )
+        t.end()
+      })
+    }
+  )
+})
+
+test.after('cleanup', t => {
+  fs.readdir(dir, (err, files) => {
+    if (err) throw err
+
+    for (const file of files) {
+      fs.unlinkSync(path.join(dir, file), err => {
+        if (err) throw err
+      })
+    }
+
+    fs.rmdirSync(dir)
+  })
+})
